9f4ed5c00518737d415b922acaf61f389f19b49f
Nizamani Adal <nizamaniabdulmalik321@gmail.com>
2025-07-12 06:18:45 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
-
+from blog.permissions import AuthorModifyOrReadOnly
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 
 
 class PostList(generics.ListCreateAPIView):
@@ -10,5 +12,10 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class IsAdminUserForObject(permissions.IsAdminUser):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and request.user.is_staff)
