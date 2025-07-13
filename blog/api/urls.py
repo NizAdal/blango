ea7c799612b81fc3dc568e5e5de6bcbdcd16f061
Nizamani Adal <nizamaniabdulmalik321@gmail.com>
2025-07-13 19:47:09 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,10 +5,15 @@ from blog.api.views import PostList, PostDetail
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
+from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -16,6 +21,7 @@ urlpatterns = [
 urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
+    path("", include(router.urls)),
     path("token-auth/", views.obtain_auth_token),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
